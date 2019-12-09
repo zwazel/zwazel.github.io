027570635628b090fc75b6ef06481b7f0bde9100
Tim Irmler <42088586+zwazel@users.noreply.github.com>
2019-12-09 15:24:16 +0100

    fixed thing with the navbar
    problem now is that i dont have any navbar

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -41,33 +41,7 @@
 
 		// Off-Canvas Navigation.
 
-			// Navigation Panel Toggle.
-				$('<a href="#navPanel" class="navPanelToggle"></a>')
-					.appendTo($body);
-
-			// Navigation Panel.
-				$(
-					'<div id="navPanel">' +
-						$('#nav').html() +
-						'<a href="#navPanel" class="close"></a>' +
-					'</div>'
-				)
-					.appendTo($body)
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'right'
-					});
-
-			// Fix: Remove transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#navPanel')
-						.css('transition', 'none');
-
-
+			
 	});
 
 })(jQuery);
